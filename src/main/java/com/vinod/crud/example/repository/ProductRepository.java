2a2a53784de45755b72ab99f8a3237a5651b0281
vinod kumar methuku <methukuvinodkumar@gmail.com>
2023-06-13 14:39:11 -0500

    first commit

---
+++ src/main/java/com/vinod/crud/example/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.vinod.crud.example.repository;
+
+import com.vinod.crud.example.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
+
